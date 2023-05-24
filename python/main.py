366d3b32aad11fd435bfe1a17e86311137df37e0
ManaasGupta <manasgpt3@gmail.com>
2023-05-24 12:17:21 +0530

    added CLI utility

--- python/main.py
+++ python/main.py
@@ -4,55 +4,33 @@ from pdf_to_docx import convert_pdf_to_docx
 from pdf_to_txt import convert_pdf_to_text
 from txt_to_docx import convert_txt_to_docx
 from txt2pdf import text_to_pdf
+import argparse
+import sys
+import os
 
-list_of_operations=["Word File to TXT File","Word File to PDF File","PDF File to Word File","PDF File to TXT File","TXT File to Word File","TXT File to PDF File"]
-
-def main():
+def main(args):
     print("Welome to Format converter\n")
-    print("Select Option number from below:\n ")
-    for idx,value in enumerate(list_of_operations):
-        print(f"{idx+1} : {value}\n")
-    op_num=int(input("Enter Operation Number : " ))
     print("\n")
-    if op_num==1:
-        print("Enter Word File Name : ",end="")
-        word_file_name=input()
-        print("Enter Output File Name : ",end="")
-        output_file_name=input()
-        convert_docx_to_txt(word_file_name,output_file_name)
-    elif op_num==2:
-        print("Enter PDF File Name : ",end="")
-        pdf_file_name=input()
-        print("Enter Output File Name : ",end="")
-        output_file_name=input()
-        docx_to_pdf(pdf_file_name,output_file_name)
-    elif op_num==3:
-        print("Enter PDF File Name : ",end="")
-        pdf_file_name=input()
-        print("Enter Output File Name : ",end="")
-        output_file_name=input()
-        convert_pdf_to_docx(pdf_file_name,output_file_name)
-    elif op_num==4:
-        print("Enter PDF File Name : ",end="")
-        pdf_file_name=input()
-        print("Enter Output File Name : ",end="")
-        output_file_name=input()
-        convert_pdf_to_text(pdf_file_name,output_file_name)
-    elif op_num==5:
-        print("Enter TXT File Name : ",end="")
-        txt_file_name=input()
-        print("Enter Output File Name : ",end="")
-        output_file_name=input()
-        convert_txt_to_docx(txt_file_name,output_file_name)
-    elif op_num==6:
-        print("Enter TXT File Name : ",end="")
-        txt_file_name=input()
-        print("Enter Output File Name : ",end="")
-        output_file_name=input()
-        text_to_pdf(txt_file_name,output_file_name)
+    oper_num=int(args.mode)
+    if oper_num==1:
+        convert_docx_to_txt(args.input_file,args.output_file)
+    elif oper_num==2:
+        docx_to_pdf(args.input_file,args.output_file)
+    elif oper_num==3:
+        convert_pdf_to_docx(args.input_file,args.output_file)
+    elif oper_num==4:
+        convert_pdf_to_text(args.input_file,args.output_file)
+    elif oper_num==5:
+        convert_txt_to_docx(args.input_file,args.output_file)
+    elif oper_num==6:
+        text_to_pdf(args.input_file,args.output_file)
     else:
         print("Invalid Operation Number !")
-        
     
 if __name__=="__main__":
-    main()
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-i','--input_file',help="Enter path to input_file you want to convert with extension",required=True)
+    parser.add_argument('-o','--output_file',help="Enter path to ouput_file you want to converted in extension",required=True)
+    parser.add_argument('-m','--mode',help="Enter path to ouput_file you want to converted in extension",required=True,type=int)
+    args=parser.parse_args()
+    sys.stdout.write(str(main(args)))
